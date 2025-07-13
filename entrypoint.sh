f4a16cfee048128a8c448b2c034f3c62194c0e84
minh130403 <tienminh130403@gmail.com>
2025-07-13 18:29:14 +0700

    updoad img

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 
 echo "📦 Running migrations..."
 # php artisan migrate --force
+php artisan config:clear
+php artisan config:cache
 
 echo "🚀 Starting Laravel server..."
 php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
