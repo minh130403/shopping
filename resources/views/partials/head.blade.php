f0ff6df06ecddac158e604d6f390e5ff28fd5bea
minh130403 <tienminh130403@gmail.com>
2025-07-13 18:49:30 +0700

    updoad img

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,3 +1,6 @@
+ <script>
+    window.livewire_app_url = "{{ config('app.url') }}";
+</script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
